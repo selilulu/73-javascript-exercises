aefb020265ff16f17ffb4086204ba31994408e84
Selin Tezel <selin.tezel@hotmail.com>
2020-11-09 15:52:57 +0100

    add prompt

---
+++ 01-base/02-var-prompt/script.js
@@ -0,0 +1,4 @@
+
+const person = prompt("What is your name","name");
+//const and let variables can also used as variables so instead of the var.Rememember that const is not changable but let might be changed to another name by new updates of the javascript language.
+alert("Hello " + person + "!");
