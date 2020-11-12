5b7f2a354563e1e96ab3a3b7e1f5a2c8f94b0988
Selin Tezel <selin.tezel@hotmail.com>
2020-11-12 10:39:41 +0100

    Use fizzbuzz

--- 02-maths/04-sort-numbers/script.js
+++ 02-maths/04-sort-numbers/script.js
@@ -14,15 +14,12 @@
     // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("run").addEventListener("click", function() {
-
-        // my code works with the run id element not with numbers element ?
+//my code here
         var points = [2, 4, 14, 10, 90, 23, 16];
-        document.getElementById("numbers").innerHTML = points;
-        function myFunction() {
 
         points.sort(function(a, b){return a - b});
-        document.getElementById("numbers").innerHTML = points;
-        }
+        document.getElementById("numbers").value = points;
+        
 
     });
 
---
+++ 03-colors/01-change-bcg-one/script.js
@@ -0,0 +1,21 @@
+/* becode/javascript
+ *
+ * /03-colors/01-change-bcg-one/script.js - 3.1: couleur de fond (1)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    // your code here
+    function myFunction() {
+        document.getElementById("red").style.backgroundcolor = "#ff0000";
+        document.getElementById("green").style.color = "green";
+        document.getElementById("blue").style.color = "blue";
+        document.getElementById("yellow").style.color = "yellow";
+      }
+})();
