fff3afe35a19948157b28628dc279197eb514a41
Selin Tezel <selin.tezel@hotmail.com>
2020-11-26 09:34:41 +0100

    Use loop

--- 07-dom/06-mini-carousel/script.js
+++ 07-dom/06-mini-carousel/script.js
@@ -10,15 +10,36 @@
 // You will have time to focus on it later.
 
 (function() {
-
-    var gallery= [
-        "../../_shared/img/bell.svg",
-        "../../_shared/img/clock.svg",
-        "../../_shared/img/compass.svg",
-        "../../_shared/img/lemon.svg",
-        "../../_shared/img/map.svg",
+    //When you click the button, change the source of the image to the next one listed in the gallery array.
+  var gallery= [
+    "../../_shared/img/bell.svg",
+    "../../_shared/img/clock.svg",
+    "../../_shared/img/compass.svg",
+    "../../_shared/img/lemon.svg",
+    "../../_shared/img/map.svg",
     ];
+  
+let currentIndex = 0; // doesnt work when its inside of the click function!
 
-    // your code here
+
+ document.getElementById("next").addEventListener("click", function(){
+
+
+   let value=document.querySelector("img");
+
+   value.src = gallery[currentIndex];
+
+
+if (currentIndex === 4){
+    currentIndex = 0; 
+}else{
+    currentIndex ++;
+    }
+
+});
 
 })();
+
+
+
+
