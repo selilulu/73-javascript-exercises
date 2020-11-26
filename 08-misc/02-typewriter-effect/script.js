65fba4a947af20c4ba796ad3cac1284bc5b34a52
Selin Tezel <selin.tezel@hotmail.com>
2020-11-26 20:03:45 +0100

    Add settimeout method

--- 08-misc/02-typewriter-effect/script.js
+++ 08-misc/02-typewriter-effect/script.js
@@ -11,6 +11,22 @@
 
 (function() {
 
+    let text= "I am a text baby";
+    
+    let speed =Math.floor(Math.random() *1000);
+
+    let i = 0;
+
+
+function typeWriter() {
+  if (i < text.length ) {
+   document.getElementById("target").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter();
+
     // your code here
 
 })();
