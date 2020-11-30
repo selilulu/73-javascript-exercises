a92d5cd7515ff804068a0b6936e4f54f74a11492
Selin Tezel <selin.tezel@hotmail.com>
2020-11-30 15:41:26 +0100

    Call back

---
+++ 09-asynchronous/01-get-posts/script.js
@@ -0,0 +1,23 @@
+/* becode/javascript
+ *
+ * /10-asynchronous/01-get-posts/script.js - 10.1: chargement d'articles
+ *
+ * coded by leny@BeCode
+ * started at 09/05/2019
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(() => {
+    // your code here
+    document.getElementById("run").addEventListener("click", function(){
+
+        window.lib.getPosts(function(error, articles){
+
+            console.log(error, articles);
+
+        });
+    })
+
+})();
